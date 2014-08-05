ab28940696af3241d248e4e96d6dab3b83e8f5a7
Casey Kolderup <casey.kolderup@voxmedia.com>
2014-08-04 23:35:05 -0400

    actually put the config vars in the client

--- bots.rb
+++ bots.rb
@@ -3,10 +3,10 @@
 require 'twitter_ebooks'
 
 Ebooks::Bot.new("wow-rude") do |bot|
-  bot.consumer_key = "" # Your app consumer key
-  bot.consumer_secret = "" # Your app consumer secret
-  bot.oauth_token = "" # Token connecting the app to this account
-  bot.oauth_token_secret = "" # Secret connecting the app to this account
+  bot.consumer_key = ENV['TWITTER_CONSUMER_KEY']
+  bot.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+  bot.oauth_token = ENV['TWITTER_OAUTH_TOKEN']
+  bot.oauth_token_secret = ENV['TWITTER_OAUTH_SECRET']
 
   bot.on_follow do |user|
     bot.follow(user[:screen_name])
